7ec7744bab09de182d2200d7981788a2f1f2f51c
Praksh671 <prakashsochandrasekar@gmail.com>
2024-11-05 10:02:18 +0530

    updating with right Scss and routes

--- css-basic/src/app/app.routes.ts
+++ css-basic/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { SelectorComponent } from './selector/selector.component';
+import { PesudosComponent } from './selector/pesudos/pesudos.component';
+import { MidSelectorComponent } from './selector/mid-selector/mid-selector.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'selector',
+        component: SelectorComponent
+    },
+    {
+        path: 'pseudos',
+        component: PesudosComponent
+    },
+    {
+     path: 'mid-selector',
+     component: MidSelectorComponent
+    }
+];
